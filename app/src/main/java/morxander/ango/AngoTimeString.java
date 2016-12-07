d532873fd725d39e8d99038393deb2635d159935
Morad Edwar <me@morad-edwar.com>
2016-12-07 14:36:12 +0200

    Converting AngoTimeString to singleton class

--- app/src/main/java/morxander/ango/AngoTimeString.java
+++ app/src/main/java/morxander/ango/AngoTimeString.java
@@ -5,9 +5,62 @@ package morxander.ango;
  */
 
 public class AngoTimeString {
-    public static final String NOW = "Just Now";
-    public static final String MINUTE = "minute";
-    public static final String MINUTES = "minutes";
-    public static final String AGO = "ago";
-    public static final String ONE_MINUTE = "1 " + MINUTE + " " + AGO;
+
+    private static String now = "Just Now";
+    private String minute = "minute";
+    private String minutes = "minutes";
+    private String ago = "ago";
+    private String one_minute = "1 " + minute + " " + ago;
+
+    private static AngoTimeString angoTimeString;
+
+    private AngoTimeString() {
+    }
+
+    public static AngoTimeString getInstance() {
+        if(angoTimeString == null){
+            angoTimeString = new AngoTimeString();
+        }
+        return angoTimeString;
+    }
+
+    public String getNow() {
+        return now;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public String getMinutes() {
+        return minutes;
+    }
+
+    public String getAgo() {
+        return ago;
+    }
+
+    public String getOne_minute() {
+        return one_minute;
+    }
+
+    public void setNow(String now) {
+        this.now = now;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public void setMinutes(String minutes) {
+        this.minutes = minutes;
+    }
+
+    public void setAgo(String ago) {
+        this.ago = ago;
+    }
+
+    public void setOne_minute(String one_minute) {
+        this.one_minute = one_minute;
+    }
 }
